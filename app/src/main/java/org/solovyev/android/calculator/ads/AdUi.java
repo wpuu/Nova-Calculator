7f22f0a3b79c207b4b27dff4455da7c7b12fb9c5
wpuu <xgyk@163.com>
2026-08-30 06:01:33 +0800

    Disable inherited ad entitlement flow for commercial testing

--- app/src/main/java/org/solovyev/android/calculator/ads/AdUi.java
+++ app/src/main/java/org/solovyev/android/calculator/ads/AdUi.java
@@ -1,113 +1,57 @@
 package org.solovyev.android.calculator.ads;
 
-import static org.solovyev.android.checkout.ProductTypes.IN_APP;
-
-import android.os.Handler;
-import android.os.Looper;
-import androidx.annotation.NonNull;
 import android.view.View;
+
+import androidx.annotation.NonNull;
 
 import org.solovyev.android.calculator.AdView;
 import org.solovyev.android.calculator.R;
-import org.solovyev.android.checkout.CppCheckout;
-import org.solovyev.android.checkout.Inventory;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 
-
+/**
+ * No-op advertising coordinator for the Nova commercial branch.
+ *
+ * A Nova-owned advertising/consent implementation will replace this later. Keeping the
+ * inherited view lifecycle API avoids touching unrelated calculator fragments while ensuring
+ * that old Calculator++ billing and AdMob identities are not exercised during commercial tests.
+ */
 public class AdUi {
 
-    @NonNull
-    private final CppCheckout checkout;
-    @NonNull
-    private final Handler handler;
     @Nullable
     AdView adView;
-    @Nullable
-    private Boolean adFree = null;
 
     @Inject
-    public AdUi(@NonNull CppCheckout checkout, @NonNull Handler handler) {
-        this.checkout = checkout;
-        this.handler = handler;
+    public AdUi() {
     }
 
     public void onCreate() {
-        checkout.start();
     }
 
     public void onResume() {
-        if (adView == null) {
-            return;
-        }
-        adView.resume();
-        if (adFree != null) {
-            updateAdView();
-        } else {
-            checkout.loadInventory(Inventory.Request.create().loadAllPurchases(),
-                    onMainThread(new Inventory.Callback() {
-                        @Override
-                        public void onLoaded(@Nonnull Inventory.Products products) {
-                            adFree = products.get(IN_APP).isPurchased("ad_free");
-                            updateAdView();
-                        }
-                    }));
-        }
-    }
-
-    private void updateAdView() {
-        if (adFree == null || adView == null) {
-            return;
-        }
-
-        if (adFree) {
+        if (adView != null) {
             adView.hide();
-        } else {
-            adView.show();
         }
-    }
-
-    @Nonnull
-    private Inventory.Callback onMainThread(@Nonnull final Inventory.Callback callback) {
-        return new Inventory.Callback() {
-            @Override
-            public void onLoaded(@Nonnull final Inventory.Products products) {
-                if (handler.getLooper() == Looper.myLooper()) {
-                    callback.onLoaded(products);
-                    return;
-                }
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        callback.onLoaded(products);
-                    }
-                });
-            }
-        };
     }
 
     public void onCreateView(@NonNull View view) {
         adView = view.findViewById(R.id.cpp_ad);
+        if (adView != null) {
+            adView.hide();
+        }
     }
 
     public void onPause() {
-        adFree = null;
-        if (adView != null) {
-            adView.pause();
-        }
     }
 
     public void onDestroyView() {
-        if (adView == null) {
-            return;
+        if (adView != null) {
+            adView.destroy();
+            adView = null;
         }
-        adView.destroy();
-        adView = null;
     }
 
     public void onDestroy() {
-        checkout.stop();
     }
 }
