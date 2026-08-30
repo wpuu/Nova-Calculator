aff861ec9f0604c12582f28eb6fae284a16ca207
wpuu <xgyk@163.com>
2026-08-30 18:45:55 +0800

    Expose billing coordinator to lightweight paywall preferences

--- app/src/main/java/org/solovyev/android/calculator/AppComponent.java
+++ app/src/main/java/org/solovyev/android/calculator/AppComponent.java
@@ -1,6 +1,7 @@
 package org.solovyev.android.calculator;
 
 import dagger.Component;
+import org.solovyev.android.calculator.billing.NovaBillingCoordinator;
 import org.solovyev.android.calculator.converter.ConverterFragment;
 import org.solovyev.android.calculator.errors.FixableErrorFragment;
 import org.solovyev.android.calculator.errors.FixableErrorsActivity;
@@ -68,4 +69,6 @@ public interface AppComponent {
     void inject(PreferencesFragment fragment);
     void inject(WizardFragment fragment);
     void inject(FloatingCalculatorBroadcastReceiver receiver);
+
+    NovaBillingCoordinator billingCoordinator();
 }
